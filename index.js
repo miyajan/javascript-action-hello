f0b0c574e0f408770efe6c444c21b397a014eda8
miyajan <miyajan777@gmail.com>
2020-02-15 11:13:55 +0900

    test

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("Hello, World!");
 console.log(Object.keys(process.env));
+console.log('::set-output name=test::foo');
+console.log('::set-output name=hoge::fuga');
+console.log('::set-output name=result::true');
